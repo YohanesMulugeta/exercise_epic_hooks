d7bbc97595a4ee0b7830892d9f4ba23530bc3181
Yohanes <yohanesmulugeta21@gmail.com>
2022-07-15 18:20:56 +0300

    replace ErrorBoundary component with the boundary component from react-error-boundary package

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,6 +2,7 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
+import {ErrorBoundary} from 'react-error-boundary'
 // 🐨 you'll want the following additional things from '../pokemon':
 // fetchPokemon: the function we call to get the pokemon info
 // PokemonInfoFallback: the thing we show while we're loading the pokemon info
@@ -13,34 +14,13 @@ import {
   fetchPokemon,
 } from '../pokemon'
 
-class ErrorBaoundary extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {hasError: false, error: null}
-  }
-
-  static getDerivedStateFromError(error) {
-    return {hasError: true, error}
-  }
-
-  // componentDidCatch(error, errorInfo) {
-  //   // this.setState({error: error})
-
-  // }
-
-  render() {
-    if (this.state.hasError) {
-      return (
-        <div role="alert">
-          {' '}
-          Something happend:
-          <pre style={{whiteSpace: 'normal'}}>{this.state.error.message}</pre>
-        </div>
-      )
-    }
-    return this.props.children
-  }
+function errorFallBack({error, resetErrorBoundary}) {
+  return (
+    <div role="alert">
+      <p>Something happend:</p>
+      <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+    </div>
+  )
 }
 
 function PokemonInfo({pokemonName}) {
@@ -94,9 +74,12 @@ function App() {
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <ErrorBaoundary key={pokemonName}>
+        <ErrorBoundary
+          FallbackComponent={errorFallBack}
+          resetKeys={pokemonName}
+        >
           <PokemonInfo pokemonName={pokemonName} />
-        </ErrorBaoundary>
+        </ErrorBoundary>
       </div>
     </div>
   )
